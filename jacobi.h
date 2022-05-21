fcc9324bb94f848ec194234b4656fbd9a58b4bae
Marcos Pietrucci <marcospietrucci@gmail.com>
2022-05-21 15:15:35 -0300

    Adding more comments to the code

--- jacobi.h
+++ jacobi.h
@@ -3,10 +3,17 @@
 
 #include "common.h"
 
+//Add the initial guess (0,0,0,.... by default)
 void initial_approximation(v_type* vec_solution, int length);
-bool iterate(v_type** matrix_A, v_type* vec_B, v_type* vec_solution, int length, v_type tolerance);
-void verify_method(v_type** matrix_A, v_type* vec_B, v_type* vec_solution, int length);
-bool check_if_possible(v_type** matrix_A, int length);
 
+//Executing the method, line by line
+bool iterate(v_type** matrix_A, v_type* vec_B, v_type* vec_solution, int length, v_type tolerance);
+
+//Function that verify how precise the method is.
+//Multiply the matrix by vec_solutions and compare it to vec_B
+void verify_method(v_type** matrix_A, v_type* vec_B, v_type* vec_solution, int length);
+
+//Check if the generated matrix supports the jacobi method
+bool check_if_possible(v_type** matrix_A, int length);
 
 #endif
